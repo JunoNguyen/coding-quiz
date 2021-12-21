a1cb6877f5463eea481afb76773813c6ade61e4a
Juno <junonguyen27@gmail.com>
2021-12-21 15:03:48 -0600

    styling the coding quiz

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,37 @@
+function quizStart() {
+
+}
+
+function viewHighscores() {
+
+}
+
+var questions = [
+    {
+        question: "Placeholder1",
+        answers: {
+            A: "1",
+            B: "2",
+            C: "3",
+        },
+        correctAnswer:"B"
+    },
+    {
+        question: "Placeholder2",
+        answers: {
+            A: "1",
+            B: "2",
+            C: "3",
+        },
+        correctAnswer:"B"
+    },
+    {
+        question: "Placeholder3",
+        answers: {
+            A: "1",
+            B: "2",
+            C: "3",
+        },
+        correctAnswer:"B"
+    }
+];
