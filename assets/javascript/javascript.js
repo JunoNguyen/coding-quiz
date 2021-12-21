f848cde164fe56e31781afa84a1b4b6a491870da
Juno <junonguyen27@gmail.com>
2021-12-21 15:25:04 -0600

    finished style.css, now working on javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,9 @@
+var startButton = document.querySelector('#start')
+
+startButton.addEventListener('click', quizStart)
+
 function quizStart() {
+    startButton.classList.add('hidden')
 
 }
 
@@ -6,32 +11,36 @@ function viewHighscores() {
 
 }
 
-var questions = [
-    {
-        question: "Placeholder1",
-        answers: {
-            A: "1",
-            B: "2",
-            C: "3",
-        },
-        correctAnswer:"B"
-    },
-    {
-        question: "Placeholder2",
-        answers: {
-            A: "1",
-            B: "2",
-            C: "3",
-        },
-        correctAnswer:"B"
-    },
-    {
-        question: "Placeholder3",
-        answers: {
-            A: "1",
-            B: "2",
-            C: "3",
-        },
-        correctAnswer:"B"
-    }
-];
+function rightAnswer() {
+
+}
+
+// var questions = [
+//     {
+//         question: "Placeholder1",
+//         answers: {
+//             A: "1",
+//             B: "2",
+//             C: "3",
+//         },
+//         correctAnswer:"B"
+//     },
+//     {
+//         question: "Placeholder2",
+//         answers: {
+//             A: "1",
+//             B: "2",
+//             C: "3",
+//         },
+//         correctAnswer:"B"
+//     },
+//     {
+//         question: "Placeholder3",
+//         answers: {
+//             A: "1",
+//             B: "2",
+//             C: "3",
+//         },
+//         correctAnswer:"B"
+//     }
+// ];
